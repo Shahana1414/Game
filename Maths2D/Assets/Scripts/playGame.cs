4cce526844db296f027790da072c09f1a655e3a2
Shahana1414 <81530363+Shahana1414@users.noreply.github.com>
2021-04-04 18:45:37 +0400

    main
    main +levels

---
+++ Maths2D/Assets/Scripts/playGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playGame : MonoBehaviour
+{
+    public void Change_scene(string Level)
+    {
+        SceneManager.LoadScene(Level);
+    }
+    public void Change_Add(string Addition)
+    {
+        SceneManager.LoadScene(Addition);
+    }
+    public void Change_Subtract(string Subtraction)
+    {
+        SceneManager.LoadScene(Subtraction);
+    }
+    public void Change_Multiplication(string Multiplication)
+    {
+        SceneManager.LoadScene(Multiplication);
+    }
+    public void Change_Division(string Division)
+    {
+        SceneManager.LoadScene(Division);
+    }
+    public void Change_All(string All)
+    {
+        SceneManager.LoadScene(All);
+    }
+    public void Back(string Main)
+    {
+        SceneManager.LoadScene(Main);
+    }
+
+}
